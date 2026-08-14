938765605fba86b79d7c04f796f59656b65d6b49
taiwanwan64 <taiwanwan64@gmail.com>
2026-08-14 12:28:22 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const APP_VERSION = 'v73';
-const CACHE_NAME = 'fe-quest-v73-1';
+const APP_VERSION = 'v74';
+const CACHE_NAME = 'fe-quest-v74-1';
 const CACHE_PREFIX = 'fe-quest-';
 const APP_SHELL = [
   './',
