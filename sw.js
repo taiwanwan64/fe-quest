072a4daa0496e72202c55367b0efc9ac9e97d8d4
taiwanwan64 <taiwanwan64@gmail.com>
2026-08-15 22:36:21 +0900

    Bump FE QUEST service worker to v151

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const APP_VERSION = 'v150';
-const CACHE_NAME = 'fe-quest-v150-1';
+const APP_VERSION = 'v151';
+const CACHE_NAME = 'fe-quest-v151-1';
 const CACHE_PREFIX = 'fe-quest-';
 const APP_SHELL = [
   './',
