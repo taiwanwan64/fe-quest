6c97f9e9dfe4932f6a7408712af9003605b8efa8
taiwanwan64 <taiwanwan64@gmail.com>
2026-08-15 18:30:34 +0900

    Update FE QUEST v143 service worker

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const APP_VERSION = 'v142';
-const CACHE_NAME = 'fe-quest-v142-1';
+const APP_VERSION = 'v143';
+const CACHE_NAME = 'fe-quest-v143-1';
 const CACHE_PREFIX = 'fe-quest-';
 const APP_SHELL = [
   './',
