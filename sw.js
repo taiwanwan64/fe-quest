ae18d1bc94c4294a612e5977c65071cd2092710d
taiwanwan64 <taiwanwan64@gmail.com>
2026-08-16 10:27:08 +0900

    Bump service worker cache to v160

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const APP_VERSION = 'v159';
-const CACHE_NAME = 'fe-quest-v159-1';
+const APP_VERSION = 'v160';
+const CACHE_NAME = 'fe-quest-v160-1';
 const CACHE_PREFIX = 'fe-quest-';
 const APP_SHELL = [
   './',
