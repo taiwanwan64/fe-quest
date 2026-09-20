a8a909351eb763a6b858703bfe91c533b58800e8
taiwanwan64 <taiwanwan64@gmail.com>
2026-09-20 21:07:57 +0900

    科目B第1章文法CSSをPWAキャッシュへ追加

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const APP_VERSION = 'v377';
-const CACHE_NAME = 'fe-quest-v377-84';
+const CACHE_NAME = 'fe-quest-v377-85';
 const CACHE_PREFIX = 'fe-quest-';
 const APP_SHELL = [
   "./",
@@ -27,6 +27,7 @@ const APP_SHELL = [
   "./assets/ch20-depth-v401.css",
   "./assets/ch21-depth-v402.css",
   "./assets/ch22-strategy-v403.css",
+  "./assets/bbook-ch01-grammar-v404.css",
   "./assets/protected-content-provider-v376.js",
   "./assets/protected-content-provider-v376-v7.js",
   "./assets/protected-content-provider-v376-v8.js",
