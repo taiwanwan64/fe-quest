b450b6f45408b79d13ef0ba5f98a4a3d95d92948
taiwanwan64 <taiwanwan64@gmail.com>
2026-09-21 12:16:02 +0900

    科目B第10章ガイドをPWAキャッシュへ追加

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const APP_VERSION = 'v377';
-const CACHE_NAME = 'fe-quest-v377-93';
+const CACHE_NAME = 'fe-quest-v377-94';
 const CACHE_PREFIX = 'fe-quest-';
 const APP_SHELL = [
   "./",
@@ -36,6 +36,7 @@ const APP_SHELL = [
   "./assets/bbook-ch07-oop-v410.css",
   "./assets/bbook-ch08-list-v411.css",
   "./assets/bbook-ch09-stackqueue-v412.css",
+  "./assets/bbook-ch10-bitstring-v413.css",
   "./assets/protected-content-provider-v376.js",
   "./assets/protected-content-provider-v376-v7.js",
   "./assets/protected-content-provider-v376-v8.js",
